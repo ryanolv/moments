2b01e48662dcb4dc4c160b6b9084c6c02b24dcd0
Ryan da Silva Oliveira <ryanprofissional02@gmail.com>
2023-05-31 15:02:56 -0300

    Finalizando navegação de poup-up de mensagem do sistema

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { Moment } from 'src/app/Moments';
+
 import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
 
 @Component({
   selector: 'app-new-moment',
@@ -10,7 +14,11 @@ import { MomentService } from 'src/app/services/moment.service';
 export class NewMomentComponent implements OnInit {
   btnText = "Compartilhar";
 
-  constructor(private momentService: MomentService) {}
+  constructor(
+    private momentService: MomentService,
+    private messagesService: MessagesService,
+    private router: Router
+    ) {}
 
   ngOnInit(): void {}
 
@@ -24,12 +32,15 @@ export class NewMomentComponent implements OnInit {
       formData.append('image', moment.image);
     }
 
-    // todo
+    // to do
 
     // enviar para o service
     await this.momentService.createMoment(formData).subscribe();
-    // exibir msg
 
+    // exibir msg
+    this.messagesService.add('Momento adicionado com sucesso!');
+    
     // redirect
+    this.router.navigate(['/'])
   }
 }
