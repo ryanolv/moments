1457d609ae8a156dd4028cfb714f3b38afa34d68
Ryan da Silva Oliveira <ryanprofissional02@gmail.com>
2023-05-31 13:04:04 -0300

    Criando componente para mensagem do sistema

---
+++ src/app/components/messages/messages.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent {
+
+  faTimes = faTimes;
+
+}
